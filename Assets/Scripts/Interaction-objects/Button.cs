c0ffa286c147d7c593eaaf04ee49d7948e7758bf
MrBONAN <taleshatrycob@gmail.com>
2024-05-12 15:37:28 +0500

    Добавил префаб лифта

--- Assets/Scripts/Interaction-objects/Button.cs
+++ Assets/Scripts/Interaction-objects/Button.cs
@@ -2,50 +2,53 @@ using System.Linq;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Button : MonoBehaviour
+namespace Interaction_objects
 {
-    public int turnOn;
-    private Animator animator;
-    public UnityEvent onPressed;
-    public UnityEvent onStayPressed;
-    public UnityEvent onReleased;
-    private string[] whoCanInteract = { "Big player" };
-
-    private void Start()
+    public class Button : MonoBehaviour
     {
-        animator = GetComponentInChildren<Animator>();
-    }
+        public int turnOn;
+        private Animator animator;
+        public UnityEvent onPressed;
+        public UnityEvent onStayPressed;
+        public UnityEvent onReleased;
+        private string[] whoCanInteract = { "Big player" };
 
-    private void FixedUpdate()
-    {
-        animator.SetBool("Pressed", turnOn != 0);
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (CheckIfCanInteract(other))
+        private void Start()
         {
-            turnOn += 1;
-            onPressed.Invoke();
-            Debug.Log("Button pressed");
+            animator = GetComponentInChildren<Animator>();
         }
-    }
 
-    private void OnTriggerStay2D(Collider2D other)
-    {
-        if (CheckIfCanInteract(other))
-            onStayPressed.Invoke();
-    }
-
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (CheckIfCanInteract(other))
+        private void FixedUpdate()
         {
-            turnOn -= 1;
-            onReleased.Invoke();
+            animator.SetBool("Pressed", turnOn != 0);
         }
-    }
 
-    private bool CheckIfCanInteract(Collider2D other)
-        => whoCanInteract.Contains(other.gameObject.tag);
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (CheckIfCanInteract(other))
+            {
+                turnOn += 1;
+                onPressed.Invoke();
+                Debug.Log("Button pressed");
+            }
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (CheckIfCanInteract(other))
+                onStayPressed.Invoke();
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (CheckIfCanInteract(other))
+            {
+                turnOn -= 1;
+                onReleased.Invoke();
+            }
+        }
+
+        private bool CheckIfCanInteract(Collider2D other)
+            => whoCanInteract.Contains(other.gameObject.tag);
+    }
 }
