6682c141d2f747d2994a6abe51ce2b472c4a953f
MrBONAN <taleshatrycob@gmail.com>
2024-05-17 22:19:05 +0500

    Добавлен префаб MiniGamesHandler

--- Assets/Scripts/MiniGames/MiniGamesHandler/MiniGamesHandler.cs
+++ Assets/Scripts/MiniGames/MiniGamesHandler/MiniGamesHandler.cs
@@ -8,14 +8,14 @@ namespace MiniGames.MiniGamesZone
 
         [SerializeField] private Camera zoneCamera;
         private IMiniGame currentMiniGame;
-        [SerializeField] private MazeMiniGame.MazeObject mazeGame;
+        private MazeMiniGame.MazeObject mazeGame;
 
         public bool IsMiniGameActive
             => currentMiniGame is not null;
 
         private void Awake()
         {
-            zoneCamera = GetComponent<Camera>();
+            zoneCamera = GetComponentInChildren<Camera>();
             zoneCamera.enabled = false;
 
             zoneCamera.rect = new Rect(0.1f, 0.1f, 0.9f, 0.9f);
