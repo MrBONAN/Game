f1517a21c4902dc18c8674d7bd873e84df458595
MrBONAN <taleshatrycob@gmail.com>
2024-05-17 20:58:03 +0500

    Создан объект обработчика событий игры

---
+++ Assets/Scripts/GameHandler/GameHandler.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEngine;
+
+namespace GameHandler
+{
+    public class GameHandler : MonoBehaviour
+    {
+        [SerializeField] private Player1 firstPlayerPrefab;
+        [SerializeField] private Player2 secondPlayerPrefab;
+        [SerializeField] private Camera camera1;
+        [SerializeField] private Camera camera2;
+        [SerializeField] private MiniGames.MiniGamesZone.MiniGamesHandler miniGamesHandler;
+        
+        private bool 
+
+        private void Update()
+        {
+            
+        }
+    }
+}
