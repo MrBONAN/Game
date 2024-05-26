afc053d97347771a1258a14cfaa4dc3dd30234f7
Andrew <androlimp22@gmail.com>
2024-05-26 12:10:15 +0500

    Saving wires with prepared code

---
+++ Assets/Scripts/MiniGames/Wires/Wire.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace MazeMiniGame
+{
+    public enum VisitedState
+    {
+        Visited,
+        Unvisited
+    }
+
+    public enum WireType
+    {
+        Default,
+        Long,
+        Corner,
+        LongCorner,
+        Bridge
+    }
+    
+    public Dictionary<WireType, (int Width, int Height)> WireSize;
+    
+    public class Wire
+    {
+        public Vector2Int Entry;
+        public Vector2Int Exit;
+        private WireType _type;
+        private WireGUI WireGUI;
+
+        public WireType Type
+        {
+            get { return _type;}
+            set
+            {
+                _type = value;
+                WireGUI.Type = value;
+            }
+        }
+
+        public void SetPosition(Vector2Int positionOfEntry, Rotation rotation, WireType type)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/MiniGames/Wires/WireGUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Numerics;
+using UnityEngine;
+
+namespace MazeMiniGame
+{
+    public enum Rotation
+    {
+        Normal,
+        Degree90,
+        Degree180,
+        Degree270
+    }
+    public class WireGUI
+    {
+        public WireType Type;
+        public Vector2Int Position;
+        public Rotation Rotation;
+
+        public void Draw()
+        {
+            
+        }
+    }
+}
