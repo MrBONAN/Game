c96c65cfb6c8ec83fe9535ac9a8a165c32d0eef1
MrBONAN <taleshatrycob@gmail.com>
2024-05-26 22:49:31 +0500

    Наконец-то пофиксил отображение кнопок

--- Assets/Scripts/MiniGames/Lamps/Button.cs
+++ Assets/Scripts/MiniGames/Lamps/Button.cs
@@ -7,11 +7,25 @@ namespace LampsMiniGame
 {
     public class Button : MonoBehaviour
     {
+        public SpriteAtlas spriteAtlas;
         private SpriteRenderer buttonBase;
         private SpriteRenderer buttonText;
 
         private ButtonContent _bg;
         private ButtonContent _txt;
+
+        private Vector3 _pos;
+
+        public Vector3 Position
+        {
+            get => _pos;
+            set
+            {
+                _pos = value;
+                transform.localPosition = value;
+            }
+        }
+
         public ButtonContent ButtonBackground
         {
             get => _bg;
@@ -31,10 +45,8 @@ namespace LampsMiniGame
                 buttonText.sprite = spriteAtlas.GetSprite(value.ToString());
             }
         }
-        
-        public SpriteAtlas spriteAtlas;
 
-        private void Start()
+        private void Awake()
         {
             foreach (var component in GetComponentsInChildren<SpriteRenderer>())
             {
@@ -43,13 +55,20 @@ namespace LampsMiniGame
                 else if (component.name is "Text")
                     buttonText = component;
             }
+
             SetButton(ButtonContent.One, ButtonContent.Default);
         }
 
-        private void SetButton(ButtonContent txt, ButtonContent bg)
+        public void SetButton(ButtonContent txt, ButtonContent bg)
         {
             ButtonBackground = bg;
             ButtonText = txt;
+        }
+
+        public void SetButtonTextColor(ButtonContent textColor)
+        {
+            _txt = textColor;
+            buttonText.sprite = spriteAtlas.GetSprite(textColor + "-Text");
         }
     }
 }
--- Assets/Scripts/MiniGames/Lamps/MemorizingLampsMiniGame.cs
+++ Assets/Scripts/MiniGames/Lamps/MemorizingLampsMiniGame.cs
@@ -10,15 +10,23 @@ namespace LampsMiniGame
         private Lamps game;
         [SerializeField] private int levelsCount = 5;
         [SerializeField] private int levelsDifficulty = 1;
+        
+        [SerializeField] private Button ButtonPrefab;
+        private GameObject firstPlayerButtons;
+        private GameObject secondPlayerButtons;
+        private Button[,] firstPlayer = new Button[2, 4];
+        private Button[,] secondPlayer = new Button[2, 4];
 
         public override void StartMiniGame()
         {
             game = new Lamps(levelsCount, levelsDifficulty);
+            CreateButtons();
             Debug.Log("MemorizingLampsMiniGame started");
         }
 
         public override MiniGameResult UpdateMiniGame()
         {
+            //return MiniGameResult.ContinuePlay;
             foreach (var (direction, keyCode) in PlayerControl.ControlFirst)
                 if (Input.GetKeyDown(keyCode) &&
                     direction is Control.Up or Control.Down or Control.Left or Control.Right)
@@ -71,10 +79,44 @@ namespace LampsMiniGame
             return MiniGameResult.ContinuePlay;
         }
 
+        private void CreateButtons()
+        {
+            var go = new GameObject();
+            firstPlayerButtons = Instantiate(go, transform);
+            var shift = new Vector3(5, 0);
+            firstPlayerButtons.transform.position = transform.position - shift;
+            for (var i = 0; i < 8; i++)
+            {
+                var b = Instantiate(ButtonPrefab, firstPlayerButtons.transform);
+                b.SetButton((ButtonContent)((int)ButtonContent.One + i), ButtonContent.Default);
+                b.Position = new Vector3(i % 4, i / 4);
+                firstPlayer[i / 4, i % 4] = b;
+            }
+            
+            secondPlayerButtons = Instantiate(go, transform);
+            secondPlayerButtons.transform.position = transform.position + shift;
+            for (var i = 0; i < 8; i++)
+            {
+                var b = Instantiate(ButtonPrefab, secondPlayerButtons.transform);
+                b.ButtonBackground = ButtonContent.Red + i;
+                b.SetButtonTextColor(ButtonContent.Red + i);
+                b.Position = new Vector3(i % 4, i / 4);
+                secondPlayer[i / 4, i % 4] = b;
+            }
+            
+            Destroy(go);
+        }
+
         public override void OnDestroy()
         {
-            Debug.Log("MazeMiniGame destroyed");
-            throw new NotImplementedException();
+            foreach (var button in firstPlayer)
+                Destroy(button);
+            foreach (var button in secondPlayer)
+                Destroy(button);
+            Destroy(firstPlayerButtons);
+            Destroy(secondPlayerButtons);
+            
+            Debug.Log("MemorizingLampsMiniGame destroyed");
         }
     }
 }
