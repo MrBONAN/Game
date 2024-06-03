bc9f2b36765e4d5ac186bae0daa6bc69975dc8c8
Andrew <androlimp22@gmail.com>
2024-06-03 11:26:54 +0500

    added level 2

--- Assets/Scripts/MiniGames/Wires/WiresStateFormer.cs
+++ Assets/Scripts/MiniGames/Wires/WiresStateFormer.cs
@@ -69,5 +69,53 @@ namespace MazeMiniGame
             field.SetField(stringField, scale, shift, bridgePositions, bridgesRotations);
             return field;
         }
+
+        private static WireField MakeField2(GameObject gameObject, WireGUIPref wirePef, FieldGUIPref fieldPref,
+            Transform camera, GameObject start, GameObject end, GameObject line)
+        {
+            var field = gameObject.AddComponent<WireField>();
+            field.transform.position = camera.position;
+            field.cameraTransform = camera;
+            field.WirePrefab = wirePef;
+            field.fieldGUIPrefab = fieldPref;
+            field.linePref = line;
+
+            var stringField = new[]
+            {
+                "--/--/--",
+                "-?//-/?-",
+                "-/--/-/-",
+                "-////-/",
+                "-/-?///-",
+                "///-////",
+                "-/--/-/-",
+                "----?-/-",
+            };
+            var bridgesRotations = new Dictionary<(int, int), (Rotation, Rotation)>
+            {
+                { (1, 1), (Rotation.Degree270, Rotation.Degree90) },
+                { (4, 3), (Rotation.Degree180, Rotation.Normal) },
+                { (8, 5), (Rotation.Degree180, Rotation.Normal)},
+                { (1, 7), (Rotation.Degree180, Rotation.Normal)}
+            };
+            var scale = 3.5f;
+            var bridgePositions = stringField
+                .SelectMany((row, y) => row.Select((c, x) => new { Char = c, Coord = (y, x) }))
+                .Where(item => item.Char == '?')
+                .Select(item => item.Coord)
+                .ToArray();
+            var shiftX = (stringField[0].Length - 1) / 2.0f;
+            var shiftY = (stringField.Length - 1) / 2.0f;
+            var shift = new Vector2(shiftX, -shiftY);
+            field.startPosition1 = (0, 0);
+            field.startPosition2 = (8, 5);
+            field.currentPosition2 = field.startPosition2;
+            field.currentPosition1 = field.startPosition1;
+            field.endPosition = (3, 8);
+            field.StartPointPref = start;
+            field.EndPointPref = end;
+            field.SetField(stringField, scale, shift, bridgePositions, bridgesRotations);
+            return field;
+        }
     }
 }
