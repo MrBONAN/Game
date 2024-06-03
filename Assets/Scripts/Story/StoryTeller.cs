fc5b29ed0daaad628b8adec04d3248139cd3f275
MrBONAN <taleshatrycob@gmail.com>
2024-06-03 15:46:38 +0500

    Добавил диалоги

---
+++ Assets/Scripts/Story/StoryTeller.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Story
+{
+    public class StoryTeller : MonoBehaviour
+    {
+        [SerializeField] private List<Sprite> story = new();
+        [SerializeField] private Vector3 positionOutOfBound = new Vector3(0, -10f);
+        private SpriteRenderer spriteRenderer;
+        private Camera camera;
+        public float delay = 1f;
+
+        private void Awake()
+        {
+            camera = GetComponent<Camera>();
+            camera.enabled = false;
+            camera.rect = new Rect(0.05f, 0, 0.9f, 0.22f);
+            camera.orthographicSize = 1.12f;
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+            
+            StartCoroutine(Dialog(story));
+        }
+
+        public void PlayOneDialog(Sprite dialog)
+        {
+            StartCoroutine(Dialog(new () {dialog}));
+        }
+
+        private IEnumerator Dialog(List<Sprite> dialogStory)
+        {
+            yield return new WaitForSeconds(delay);
+            camera.enabled = true;
+            foreach (var dialog in dialogStory)
+            {
+                spriteRenderer.sprite = dialog;
+                while (!Input.GetKeyDown(KeyCode.Space))
+                    yield return null;
+                while (!Input.GetKeyUp(KeyCode.Space))
+                    yield return null;
+            }
+            camera.enabled = false;
+        }
+    }
+}
